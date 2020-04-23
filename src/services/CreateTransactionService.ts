68e6c21cd89a5552a5a7c8c87e73a5562666329f
Alisson Henrique <alisson-henri@live.com>
2020-04-23 13:48:34 -0300

    :sparkles: challenge finish

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,38 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('transaction type is invalid');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('you not have mmoney');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    if (!transaction) {
+      throw Error('This not able create');
+    }
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
